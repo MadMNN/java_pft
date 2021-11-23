d03f73378c09051e7ff9a06a518bafa809402a43
MadMNN <56576105+MadMNN@users.noreply.github.com>
2021-11-23 22:44:11 +0300

    добавлены несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,7 +2,32 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
+
+    String s = "str ", s1 = "ing";
+
+    System.out.println(s+s1);
+
+
     System.out.println("Hello");
+hello("1123");
+hello("123 ");
+double t3 = 4;
+    System.out.println(kvad(t3));
+
 
   }
+
+  public static double kvad(double l) {
+return l*l;
+
+  }
+
+
+
+  public static void hello(String somebody) {
+
+    System.out.println("World " + somebody);
+
+  }
+
 }
