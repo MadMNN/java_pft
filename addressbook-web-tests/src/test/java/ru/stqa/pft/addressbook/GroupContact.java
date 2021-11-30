60d1ec180fc681ae005842336dc5425d2ad4abfa
MadMNN <56576105+MadMNN@users.noreply.github.com>
2021-12-01 00:23:06 +0300

    доработка задание 4 Контакты

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupContact.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class GroupContact {
+  private final String first;
+  private final String last;
+  private final String addr;
+  private final String tel;
+  private final String email;
+
+  public GroupContact(String first, String last, String addr, String tel, String email) {
+    this.first = first;
+    this.last = last;
+    this.addr = addr;
+    this.tel = tel;
+    this.email = email;
+  }
+
+  public String getFirst() {
+    return first;
+  }
+
+  public String getLast() {
+    return last;
+  }
+
+  public String getAddr() {
+    return addr;
+  }
+
+  public String getTel() {
+    return tel;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
